e69f802ee80e6d28f0b7763edf5c47798a3a5deb
Galynin Vlad <shudderyvlados@gmail.com>
2022-03-10 15:28:05 +0300

    testNG and Sorts

---
+++ task_3/src/main/java/TestListener.java
@@ -0,0 +1,33 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+
+public class TestListener implements ITestListener {
+    private Logger log = LogManager.getRootLogger();
+
+    public void onTestStart(ITestResult iTestResult) {
+        if (iTestResult.getMethod().getMethodName().contains("Blabla")) {
+            log.info(iTestResult.getMethod().getMethodName() + " : 1");
+        }
+    }
+
+    public void onTestSuccess(ITestResult iTestResult) { }
+
+    public void onTestFailure(ITestResult iTestResult) {
+        log.info(iTestResult.getMethod().getMethodName() + " : 2");
+    }
+
+    public void onTestSkipped(ITestResult iTestResult) { }
+
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) { }
+
+    public void onStart(ITestContext iTestContext) { }
+
+    public void onFinish(ITestContext iTestContext) { }
+
+
+}
